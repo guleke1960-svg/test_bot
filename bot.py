f9ea1f8a7b08f5490dae3d3f7be86fae1483a432
guleke1960-svg <guleke1960@gmail.com>
2026-04-23 23:17:02 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,7 +146,7 @@ async def q3_handler(message: Message, state: FSMContext):
 
     await state.clear()
 
-    async def main():
+   async def main():
     if not TOKEN:
         raise ValueError("BOT_TOKEN енгізілмеген")
 
